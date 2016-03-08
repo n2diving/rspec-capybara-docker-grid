14324e0ee52e858f6836661459560191392ef1a6
Michael Hargiss <mdhargiss@gmail.com>
2016-03-07 20:31:37 -0700

    add output to spec

--- spec/features/example_spec.rb
+++ spec/features/example_spec.rb
@@ -2,7 +2,12 @@ require 'spec_helper'
 
 describe 'Loading the app under test' do
   it 'displays the home page' do
+    puts 'Capybara test started'
+
     visit '/'
     expect(page).to have_content 'Hello Docker World!'
+
+    puts 'Capybara test finished'
+    puts '¸¸♬·¯·♩¸¸♪·¯·♫¸¸Happy Dance¸¸♬·¯·♩¸¸♪·¯·♫¸¸'
   end
 end
